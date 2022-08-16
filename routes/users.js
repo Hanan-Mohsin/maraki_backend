f5b063075c5f95bc549fa5a118916f0e75601d99
Hanan-Mohsin <hananmohsin967088@gmail.com>
2022-08-16 19:56:49 +0300

    users route change

--- routes/users.js
+++ routes/users.js
@@ -22,7 +22,7 @@ router.get('/me', auth, async(req, res) => {
 });
 
 // Register a user
-router.post('/',  [auth,agent],async (req,res) => {
+router.post('/',async (req,res) => {
 	const {error} = validate(req.body);
 	if(error) return res.status(400).send(error.details[0].message);
 	
